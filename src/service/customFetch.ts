e75cf943e75a5e6b1aba1e734de680d46d305291
AndMed <andmed20@yandex.ru>
2024-01-16 10:47:12 +0300

    Add code styles

--- src/service/customFetch.ts
+++ src/service/customFetch.ts
@@ -1,71 +1,71 @@
 interface IOptions {
-  timeout: number;
-  headers: Record<string, string>;
-  method: "GET" | "POST" | "PUT" | "DELETE";
-  data: Document | XMLHttpRequestBodyInit | null | undefined;
+  timeout: number
+  headers: Record<string, string>
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE'
+  data: Document | XMLHttpRequestBodyInit | null | undefined
 }
 
-const t = () => {};
+var t = () => {}
 
 function queryStringify(data: Record<string, string>) {
-  if (typeof data !== "object") {
-    throw new Error("Data must be object");
+  if (typeof data !== 'object') {
+    throw new Error('Data must be object')
   }
 
-  const keys = Object.keys(data);
+  const keys = Object.keys(data)
   return keys.reduce((result, key, index) => {
-    return `${result}${key}=${data[key]}${index < keys.length - 1 ? "&" : ""}`;
-  }, "?");
+    return `${result}${key}=${data[key]}${index < keys.length - 1 ? '&' : ''}`
+  }, '?')
 }
 
 class HTTPTransport {
   get = (url: string, options: IOptions) => {
-    return this.request(url, { ...options, method: "GET" }, options.timeout);
-  };
+    return this.request(url, { ...options, method: 'GET' }, options.timeout)
+  }
   post = (url: string, options: IOptions) => {
-    return this.request(url, { ...options, method: "POST" }, options.timeout);
-  };
+    return this.request(url, { ...options, method: 'POST' }, options.timeout)
+  }
   put = (url: string, options: IOptions) => {
-    return this.request(url, { ...options, method: "PUT" }, options.timeout);
-  };
+    return this.request(url, { ...options, method: 'PUT' }, options.timeout)
+  }
   delete = (url: string, options: IOptions) => {
-    return this.request(url, { ...options, method: "DELETE" }, options.timeout);
-  };
+    return this.request(url, { ...options, method: 'DELETE' }, options.timeout)
+  }
 
   request = (url: string, options: IOptions, timeout = 5000) => {
-    const { headers = {}, method, data } = options;
+    const { headers = {}, method, data } = options
     return new Promise(function (resolve, reject) {
       if (!method) {
-        reject("No method");
-        return;
+        reject('No method')
+        return
       }
-      const xhr = new XMLHttpRequest();
-      const isGet = method === "GET";
-      const getData = data as unknown as Record<string, string>;
+      const xhr = new XMLHttpRequest()
+      const isGet = method === 'GET'
+      const getData = data as unknown as Record<string, string>
       xhr.open(
         method,
         isGet && !!data ? `${url}${queryStringify(getData)}` : url
-      );
+      )
 
       Object.keys(headers).forEach((key) => {
-        xhr.setRequestHeader(key, headers[key]);
-      });
+        xhr.setRequestHeader(key, headers[key])
+      })
 
       xhr.onload = function () {
-        resolve(xhr);
-      };
+        resolve(xhr)
+      }
 
-      xhr.onabort = reject;
-      xhr.onerror = reject;
+      xhr.onabort = reject
+      xhr.onerror = reject
 
-      xhr.timeout = timeout;
-      xhr.ontimeout = reject;
+      xhr.timeout = timeout
+      xhr.ontimeout = reject
 
       if (isGet || !data) {
-        xhr.send();
+        xhr.send()
       } else {
-        xhr.send(data);
+        xhr.send(data)
       }
-    });
-  };
+    })
+  }
 }
